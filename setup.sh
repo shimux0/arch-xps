49008bb482fe443a9426b438802c1fc1c2e62a06
shimux0 <142888819+shimux0@users.noreply.github.com>
2026-01-29 14:15:21 +0100

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ sudo pacman -Syyu --noconfirm
 cd $HOME
 xdg-user-dirs-update
 
-bash /tmp/arch-xps/scripts/paru.sh
+bash /tmp/arch-xps/scripts/aur.sh
 bash /tmp/arch-xps/scripts/ohmyzsh.sh
 bash /tmp/arch-xps/scripts/caelestia.sh
 bash /tmp/arch-xps/scripts/sysconfig.sh
