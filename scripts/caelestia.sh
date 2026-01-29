99c503b8197bf2292d9c1538eb1bdb9614087dfc
shimux0 <142888819+shimux0@users.noreply.github.com>
2026-01-29 12:38:15 +0000

    update caelestia

--- scripts/caelestia.sh
+++ scripts/caelestia.sh
@@ -3,5 +3,5 @@
 paru -S caelestia-cli
 paru -S caelestia-shell
 
-git clone https://github.com/shimux0/dots.git /usr/local/share/caelestia
+git clone https://github.com/shimux0/caelestia-dots.git /usr/local/share/caelestia
 fish /usr/local/share/caelestia/install.fish --noconfirm --spotify --vscode=codium --discord --zen --aur-helper=paru
