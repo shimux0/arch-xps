78b78d2a832d39ee467635d9f192ef1abe3b1e60
shimux0 <142888819+shimux0@users.noreply.github.com>
2026-01-29 12:58:47 +0000

    update skripts

--- scripts/aur.sh
+++ scripts/aur.sh
@@ -6,3 +6,5 @@ cd /tmp
 git clone https://aur.archlinux.org/paru.git
 cd /tmp/paru
 makepkg -si
+
+paru -S pokemonsay-newgenerations-git
--- scripts/caelestia.sh
+++ scripts/caelestia.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/bash
 
-paru -S caelestia-cli
-paru -S caelestia-shell
+paru -S caelestia-cli caelestia-shell caelestia-meta
 
 git clone https://github.com/shimux0/caelestia-dots.git /usr/local/share/caelestia
 fish /usr/local/share/caelestia/install.fish --noconfirm --spotify --vscode=codium --discord --zen --aur-helper=paru
